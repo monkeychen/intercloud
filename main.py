e06896618ed4f00bc37806e463497f76504165d9
monkeychen <cza55007@gmail.com>
2018-01-07 22:49:50 +0800

    support flask_restful

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+todoMap = {}
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'python world!'}
 
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todoMap[todo_id]}
+
+    def put(self, todo_id):
+        todoMap[todo_id] = request.form['data']
+        return {todo_id: todoMap[todo_id]}
+
+
+class Echo(Resource):
+    def get(self, msg):
+        return {'reply': msg}
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(Echo, '/echo/<string:msg>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
